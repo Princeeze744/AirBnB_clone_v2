549ae86545c329e0677fd2bf0181afb107ea9ff4
princeeze744 <Trade2uwin@gmail.com>
2023-11-22 23:22:58 +0100

    Fix

--- models/city.py
+++ models/city.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models import storage_type
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey 
 from sqlalchemy.orm import relationship
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     if storage_type == 'db':
         __tablename__ = 'cities'
--- models/state.py
+++ models/state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models import storage_type
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     if storage_type == 'db':
         __tablename__ = 'states'
